ea65b02e0815a02fbaf0e7378e3ee7ba848e44cf
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2023-11-02 19:13:28 +0530

    01-11-2023 Done

---
+++ Nov/501. Find Mode in Binary Search Tree.cpp
@@ -0,0 +1,36 @@
+// Problem Link: https://leetcode.com/problems/find-mode-in-binary-search-tree/description/
+
+class Solution {
+public:
+unordered_map<int, int> mp;
+
+void dfs(TreeNode* root) {
+    if(!root)
+        return;
+    
+    dfs(root->left);
+    mp[root->val]++;
+    dfs(root->right);
+}
+
+    vector<int> findMode(TreeNode* root) {
+        dfs(root);
+    
+    vector<int> result;
+    int maxFreq = 0;
+    
+    for(auto &it : mp) {
+        if(it.second > maxFreq) {
+            maxFreq = it.second;
+            result = {};
+            result.push_back(it.first);
+        } else if (it.second == maxFreq) {
+            result.push_back(it.first);
+        }
+    }
+    
+    return result;
+    
+}
+    
+};
