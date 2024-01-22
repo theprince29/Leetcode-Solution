7183d19db4132925bee6795ce82c83ca9d1c6951
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-01-22 21:55:16 +0530

    Time: 22 ms (76.68%), Space: 24.9 MB (34.62%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+    
+        vector<int>v;
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        int mask; int res = nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i-1] ==nums[i])
+            {
+                v.push_back(nums[i]);
+            }
+            else
+            {
+                res+=nums[i];
+            }
+        }
+        
+        v.push_back(((n*(n+1))/2) - res);
+        
+        
+        return v;
+    }
+};
