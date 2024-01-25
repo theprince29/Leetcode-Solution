7f6b6a110af2b800098ca98a72d56dcd7c539004
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-01-25 22:27:48 +0530

    Time: 23 ms (88.10%), Space: 25.6 MB (7.79%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+            int m = text1.length();
+    int n = text2.length();
+
+    // Create a 2D vector to store the lengths of common subsequences
+    std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, 0));
+
+    // Build the dp array
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (text1[i - 1] == text2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    // The length of the longest common subsequence is stored in dp[m][n]
+    return dp[m][n];
+
+    }
+};
