f1d80ab029f439d56bf779339f875fcdb5bc6193
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-02-13 07:32:13 +0530

    Time: 81 ms (7.01%), Space: 36.5 MB (7.01%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -16,9 +16,24 @@ public:
     }
 private:
     bool ispali(string s)
-    {
-        string x = s;
-            reverse(x.begin(),x.end());
-        return x == s;
+    { 
+        string filteredString;
+        for(char c : s) {
+        if(isalnum(c)) {
+            filteredString += tolower(c);
+        }
     }
+    
+    
+    int left = 0;
+    int right = filteredString.size() - 1;
+    while(left < right) {
+        if(filteredString[left] != filteredString[right]) {
+            return false;
+        }
+        ++left;
+        --right;
+    }
+    return true;
+}
 };
