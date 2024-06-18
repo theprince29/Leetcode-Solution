78c062608d92342b9455ee33dff387d11b2e2190
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-06-18 13:35:14 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (10.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-     int f(int ind, const std::vector<int>& nums, std::vector<int>& dp) {
-        
-        if (ind == 0) return nums[ind];
-        if (ind < 0) return 0;
+    
+    
+    int rob(vector<int>& nums) {
+    int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
 
-        
-        if (dp[ind] != -1) return dp[ind];
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
 
-        int take = nums[ind] + f(ind - 2, nums, dp);
-        int notTake = f(ind - 1, nums, dp);
+        for (int i = 2; i < n; ++i) {
+            int take = nums[i] + dp[i - 2];
+            int nottake = dp[i - 1];
+            dp[i] = std::max(take, nottake);
+        }
 
-        dp[ind] = std::max(take, notTake);
-        return dp[ind];
-    }
-
-    int rob(std::vector<int>& nums) {
-        int n = nums.size();
-        std::vector<int> dp(n, -1); // Initialize dp array with -1
-        return f(n - 1, nums, dp);  // Start from the last house
+        return dp[n - 1];
     }
 };
