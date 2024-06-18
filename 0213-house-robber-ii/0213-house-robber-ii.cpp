e392445461e49219f3d66f3026543c1ea9bbc7f0
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-06-18 14:01:04 +0530

    Time: 4 ms (33.21%), Space: 9.3 MB (49.41%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int rob1(vector<int>& nums) {
+         int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        int prev1 = nums[0];
+        int prev2 = std::max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; ++i) {
+            int current = std::max(prev2, nums[i] + prev1);
+            prev1 = prev2;
+            prev2 = current;
+        }
+
+        return prev2;
+    }
+    int rob(vector<int>& nums) {
+    vector<int>arr1;
+    vector<int>arr2;
+     
+    int n  = nums.size(); 
+
+    if(n == 0) return 0;
+
+    if(n==1) return nums[0];
+    
+    for(int i=0; i<n; i++){
+        
+        if(i!=0) arr1.push_back(nums[i]);
+        if(i!=n-1) arr2.push_back(nums[i]);
+
+
+    }
+
+     return max(rob1(arr1),rob1(arr2));
+
+    }
+
+
+};
