77235c310e8a3a43c991f1dc28fa130fcd6ff341
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-07-23 08:48:38 +0530

    Time: 12 ms (10.56%), Space: 15.3 MB (8.78%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        map<int,int>mp;
-        for(int x: nums)
-        {
-            mp[x]++;
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
         }
-        vector<pair<int,int>>vp;
-        for(auto x: mp)
-        {
-            vp.push_back({x.second,x.first});
-        }
-        sort(vp.begin(), vp.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
-            if (a.first == b.first) {
-                return a.second > b.second; 
-            }
-            return a.first < b.first; 
-        });
-        vector<int>ans;
 
-        for(auto x:vp)
-        {
-            for(int i=1; i<=x.first;i++)
-            { 
-                ans.push_back(x.second);
+        sort(nums.begin(), nums.end(), [&](int a, int b) {
+            if (freq[a] == freq[b]) {
+                return a > b;
             }
-        }
-        return ans;
+            return freq[a] < freq[b];
+        });
+
+        return nums;
     }
 };
