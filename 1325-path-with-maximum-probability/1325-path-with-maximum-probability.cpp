4c5992bc0ef37a0cf16aa7f8f250f46eb76c804e
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-08-31 22:48:19 +0530

    Time: 105 ms (73.75%), Space: 72.3 MB (23.49%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        // Graph representation: adjacency list with node and probability pairs
         vector<vector<pair<int, double>>> g(n);
         for (int i = 0; i < edges.size(); ++i) {
             int a = edges[i][0], b = edges[i][1];
@@ -10,11 +9,9 @@ public:
             g[b].push_back({a, s});
         }
 
-        // Distance (probability) array initialized to 0
         vector<double> d(n, 0.0);
         d[start_node] = 1.0;
 
-        // Priority queue for BFS/DFS, starting with the start node
         queue<pair<double, int>> q;
         q.push({1.0, start_node});
 
@@ -24,15 +21,12 @@ public:
             double w = p.first;
             int u = p.second;
 
-            // If current probability is less than recorded, skip
             if (d[u] > w) continue;
 
-            // Traverse neighbors
             for (auto& e : g[u]) {
                 int v = e.first;
                 double t = e.second;
 
-                // If the path through u offers a higher probability, update and push to queue
                 if (d[v] < d[u] * t) {
                     d[v] = d[u] * t;
                     q.push({d[v], v});
@@ -40,7 +34,7 @@ public:
             }
         }
 
-        // Return the max probability to reach end_node
+        
         return d[end_node];
     }
 };
