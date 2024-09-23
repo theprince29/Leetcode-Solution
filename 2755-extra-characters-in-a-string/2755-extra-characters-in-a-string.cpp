d4110c360d594f142fd5ed956bc4212c1e05d0d6
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-09-23 18:19:47 +0530

    Time: 162 ms (39.02%), Space: 86.8 MB (52.7%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        int n = s.length();
+        unordered_set<string> st(begin(dictionary), end(dictionary));
+        vector<int> dp(n + 1, 0);
 
-    int sol(int i , string&s , unordered_set<string>&st ,int n,vector<int>&dp)
-    {
-        if(i>= n ) return 0;
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = 1 + dp[i + 1];
 
-        if(dp[i] != -1 ) return dp[i];
-
-
-        int result = 1+ sol(i+1,s,st,n,dp);
-
-        for(int j = i  ; j<n ;j++)
-        {
-            string curr = s.substr(i,j-i+1);
-            if(st.count(curr))
-            {
-                result = min(result, sol(j+1,s,st,n,dp));
+            for (int j = i; j < n; j++) {
+                string curr = s.substr(i, j - i + 1);
+                if (st.count(curr)) {
+                    dp[i] = min(dp[i], dp[j + 1]);
+                }
             }
         }
-        return dp[i] = result;
-    }
-
-    int minExtraChar(string s, vector<string>& dictionary) {
-        int n =  s.length();
-
-        unordered_set<string> st(begin(dictionary),end(dictionary));
-        vector<int>dp(n+1,-1);
-        return sol(0 , s, st, n,dp);
+        return dp[0];
     }
 };
