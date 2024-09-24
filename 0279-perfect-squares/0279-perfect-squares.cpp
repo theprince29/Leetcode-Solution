aea69c27521ea1be08989d687432c3003212996a
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-09-24 17:23:28 +0530

    Time: 31 ms (90.63%), Space: 11.2 MB (72.97%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -31,14 +31,17 @@ private:
 
 public:
     int numSquares(int n) {
-      vector<int> dp(n + 1, INT_MAX);
+        vector<int> dp(n + 1, INT_MAX);
+
         dp[0] = 0;
-        for (int i = 1; i <= n; i++) {
-       
-            for (int j = 1; j * j <= i; j++) {
-                dp[i] = min(dp[i], dp[i - j * j] + 1);
+
+        for (int j = 1; j * j <= n; j++) {
+            int square = j * j;
+            for (int i = square; i <= n; i++) {
+                dp[i] = min(dp[i], dp[i - square] + 1);
             }
         }
+
         return dp[n];
     }
 };
