b2ab81f1e77291657dda46cd01d6c72e4de975a7
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-09-26 00:51:38 +0530

    Time: 50 ms (26.8%), Space: 47.2 MB (39.83%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,33 +1,31 @@
 class Solution {
-
-private: 
-    int fun(int ind , vector<int>coins, int amount,vector<vector<int>> &dp)
-    {
-        if(ind == 0) 
-        {
-            if(amount % coins[ind] == 0 ) return amount/coins[ind];
-
-            return 1e9;   
-        }
-
-         if (dp[ind][amount] != -1) return dp[ind][amount];
-
-        int nottake =  fun(ind-1,coins,amount,dp);
-        int take  = INT_MAX;
-        if(coins[ind] <= amount ) 
-        {
-            take = 1 + fun(ind , coins, amount - coins[ind],dp);
-        }
-
-        return dp[ind][amount]= min(take,nottake);
-
-    }    
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n  = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans =  fun(n-1,coins,amount,dp);
-        if(ans>= 1e9) return -1;
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+
+        
+        for (int tar = 0; tar <= amount; tar++) {
+            if (tar % coins[0] == 0)
+                dp[0][tar] = tar / coins[0];
+            else
+                dp[0][tar] = 1e9; 
+        }
+
+        
+        for (int i = 1; i < n; i++) { 
+            for (int tar = 0; tar <= amount; tar++) {
+                int nottake = dp[i - 1][tar]; 
+                int take = INT_MAX;
+                if (coins[i] <= tar) {
+                    take = 1 + dp[i][tar - coins[i]];
+                }
+                dp[i][tar] = min(take, nottake);
+            }
+        }
+
+        int ans = dp[n - 1][amount];
+        if (ans >= 1e9) return -1;
         return ans;
     }
 };
