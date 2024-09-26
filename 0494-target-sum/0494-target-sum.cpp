1c03842b91e09efe633907cf8ca3b2eee1d98b74
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-09-26 09:12:29 +0530

    Time: 4 ms (84.28%), Space: 14.1 MB (48.2%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,37 +1,35 @@
 class Solution {
-
-private:
-int MOD  = 1e9+7;
-    int fun(int ind, int target, vector<int>& nums,vector<vector<int>>&dp) {
-        
-        if(dp[ind][target] != -1) return dp[ind][target];
-        
-        
-        if(ind == 0) {
-            if(target == 0 && nums[ind] == 0) return 2; 
-            if(target == 0 || target == nums[0]) return 1;
-            return 0;
-        }
-        
-        int nottake = fun(ind - 1, target, nums,dp);
-        int take = 0;
-        if(nums[ind] <= target) take = fun(ind - 1, target - nums[ind], nums,dp);
-        
-        return dp[ind][target] = (nottake + take)%MOD;
-    }
-
-
 public:
     int findTargetSumWays(vector<int>& nums, int d) {
-        int ts = 0;
-        for(int i : nums) ts += i;
-        
-        int n = nums.size();
-        
-        if(ts - d < 0 || (ts - d) % 2 != 0) return 0;
-        
-        int target = (ts - d) / 2;
-        vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        return fun(n - 1, target, nums,dp);
+    int ts = 0;
+    for(int i : nums) ts += i;  
+    
+    int n = nums.size();
+    
+    
+    if(ts - d < 0 || (ts - d) % 2 != 0) return 0;
+    
+    int target = (ts - d) / 2; 
+    vector<vector<int>> dp(n, vector<int>(target + 1, 0));
+
+    
+    for(int t = 0; t <= target; t++) {
+        if(t == 0 && nums[0] == 0) dp[0][t] = 2;
+        else if(t == 0 || t == nums[0]) dp[0][t] = 1;  
+        else dp[0][t] = 0;
     }
+
+    
+    for(int i = 1; i < n; i++) {
+        for(int tar = 0; tar <= target; tar++) {
+            int notTake = dp[i - 1][tar];  
+            int take = 0;
+            if(nums[i] <= tar) take = dp[i - 1][tar - nums[i]];  
+            dp[i][tar] = notTake + take;
+        }
+    }
+
+    return dp[n - 1][target];
+}
+
 };
