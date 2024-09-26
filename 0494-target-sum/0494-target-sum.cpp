c139e8922f5da6d5b402de449ff7872b81287400
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-09-26 09:16:30 +0530

    Time: 6 ms (76.57%), Space: 14.3 MB (45.84%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -12,24 +12,27 @@ public:
     int target = (ts - d) / 2; 
     vector<vector<int>> dp(n, vector<int>(target + 1, 0));
 
+    vector<int>prev(target+1,0);
+    vector<int>curr(target+1,0);
     
     for(int t = 0; t <= target; t++) {
-        if(t == 0 && nums[0] == 0) dp[0][t] = 2;
-        else if(t == 0 || t == nums[0]) dp[0][t] = 1;  
-        else dp[0][t] = 0;
+        if(t == 0 && nums[0] == 0) prev[t] = 2;
+        else if(t == 0 || t == nums[0]) prev[t] = 1;  
+        else prev[t] = 0;
     }
 
     
     for(int i = 1; i < n; i++) {
         for(int tar = 0; tar <= target; tar++) {
-            int notTake = dp[i - 1][tar];  
+            int notTake = prev[tar];  
             int take = 0;
-            if(nums[i] <= tar) take = dp[i - 1][tar - nums[i]];  
-            dp[i][tar] = notTake + take;
+            if(nums[i] <= tar) take = prev[tar - nums[i]];  
+            curr[tar] = notTake + take;
         }
+        prev = curr;
     }
 
-    return dp[n - 1][target];
+    return prev[target];
 }
 
 };
