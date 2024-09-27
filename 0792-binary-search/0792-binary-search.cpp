caac25292cc96d8732bbfb6e8a9539d4cce000c9
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-09-27 17:59:59 +0530

    Time: 35 ms (5.53%), Space: 30.3 MB (23.48%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,20 +1,23 @@
 class Solution {
+private:
+    int fun(vector<int>& nums, int target, int l, int h) {
+        if (l > h) return -1;
+
+        int mid = l + (h - l) / 2;  
+
+        if (nums[mid] == target) {
+            return mid;
+        } 
+        else if (nums[mid] > target) {
+            return fun(nums, target, l, mid - 1);  
+        } 
+        else {
+            return fun(nums, target, mid + 1, h);  
+        }
+    }
+
 public:
     int search(vector<int>& nums, int target) {
-          int l = 0;
-        int r = nums.size() - 1;
-
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return -1; 
+        return fun(nums, target, 0, nums.size() - 1); 
     }
 };
