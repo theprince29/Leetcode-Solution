47afc62f3efa8908c632f57c9d396a0868f9297c
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-09-27 10:18:07 +0530

    Time: 34 ms (31.71%), Space: 26 MB (32.96%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -3,8 +3,8 @@ class Solution {
 private:
     int fun(int i , int j ,string &text1 ,string &text2,vector<vector<int>>&dp)
     {
-        if(i < 0 || j <0) return 0 ; 
-        if(text1[i] == text2[j]) return dp[i][j] =  1+ fun(i-1,j-1,text1,text2,dp);
+        if(i == 0 || j == 0) return 0 ; 
+        if(text1[i-1] == text2[j-1]) return dp[i][j] =  1+ fun(i-1,j-1,text1,text2,dp);
 
         if(dp[i][j]!=-1) return dp[i][j];
 
@@ -18,6 +18,9 @@ public:
         int n1 = text1.length();
         int n2 = text2.length();
         vector<vector<int>>dp(n1+1,vector<int>(n2+1,-1));
-        return fun(n1-1,n2-1,text1,text2,dp);
+
+
+
+        return fun(n1,n2,text1,text2,dp);
     }
 };
