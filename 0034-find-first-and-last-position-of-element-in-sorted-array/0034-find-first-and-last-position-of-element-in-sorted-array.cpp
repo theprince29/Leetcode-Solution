8500ed1599cb5fe1d2c6784aea0c9c212e7b1720
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-10-04 09:44:30 +0530

    Time: 7 ms (47.67%), Space: 17 MB (5.18%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,47 +1,53 @@
-
 class Solution {
+
+private:
+    int lowerBound(vector<int>nums,int target)
+    {
+        int  l = 0;
+        int  h = nums.size() -1;
+        int ans = nums.size() ; 
+        while(l<= h)
+        {
+            int mid = (l+h)>>1;
+            if(nums[mid]>=target)
+            {
+                ans = mid;
+                h = mid-1;
+            }
+            else
+            {
+                l = mid+1;
+            }
+        }
+        return ans;
+    }    
+
+
+    int uppperBound(vector<int>nums,int target)
+    {
+        int l = 0 ;
+        int h = nums.size() -1 ;
+        int ans = nums.size() ;
+        while(l<=h)
+        {
+            int mid = (l+h)>>1;
+            if (nums[mid] > target) {
+                ans = mid;
+                h = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-     int low=0;
-        int high=nums.size()-1;
-        int fpos=-1;
-        int lpos=-1;
-        while(low<=high)
-        {
-            int mid=(low)+(high-low)/2;
-            if(nums[mid]==target)
-            {
-                fpos=mid;
-                high=mid-1;   
-            }
-            else if(nums[mid]<target)
-            {
-                low =mid+1;   
-            }
-            else
-            {
-                high=mid-1;
-            }   
-        }
+        int lb = lowerBound(nums,target);
+        int n = nums.size();
+        if(lb == n || nums[lb] != target) return {-1,-1};
+
+        return {lb,uppperBound(nums,target)-1};
         
-       low=0;
-        high=nums.size()-1;
-        
-         while(low<=high)
-        {
-            int mid=(low)+(high-low)/2;
-            
-            if(nums[mid]<=target)
-            {
-                if(nums[mid]==target)
-                    lpos=mid;
-                low=mid+1;   
-            }
-            else
-            {
-                high=mid-1;
-            }   
-        }
-        return {fpos,lpos};
     }
 };
