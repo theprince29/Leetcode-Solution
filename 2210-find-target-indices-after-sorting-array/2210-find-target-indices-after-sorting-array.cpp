76bb5fc5c1d6da0256b9e1fb54613635801d234a
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-10-04 10:11:46 +0530

    Time: 6 ms (28.92%), Space: 14.6 MB (10.23%) - LeetHub

--- 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.cpp
+++ 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.cpp
@@ -1,52 +1,18 @@
 class Solution {
 public:
-    
-int findLeft(const vector<int>& nums, int target) {
-    int left = 0, right = nums.size();
-    while (left < right) {
-        int mid = left + (right - left) / 2;
-        if (nums[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid;
+    vector<int> targetIndices(vector<int>& nums, int target) {
+          vector<int> result;
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==target)
+            {
+                result.push_back(i);
+            }
         }
-    }
-    return left;
-}
 
-
-int findRight(const vector<int>& nums, int target) {
-    int left = 0, right = nums.size();
-    while (left < right) {
-        int mid = left + (right - left) / 2;
-        if (nums[mid] <= target) {
-            left = mid + 1;
-        } else {
-            right = mid;
-        }
-    }
-    return left;
-}
-
-vector<int> targetIndices(vector<int>& nums, int target) {
+        return result;
     
-    sort(nums.begin(), nums.end());
-
-    
-    int leftIdx = findLeft(nums, target);
-    int rightIdx = findRight(nums, target);
-
-    
-    if (leftIdx == rightIdx) {
-        return {};
     }
-
-    
-    vector<int> result;
-    for (int i = leftIdx; i < rightIdx; ++i) {
-        result.push_back(i);
-    }
-
-    return result;
-}
 };
