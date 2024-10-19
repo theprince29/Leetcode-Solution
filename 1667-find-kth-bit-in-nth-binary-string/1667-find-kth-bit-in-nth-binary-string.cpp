29ca706768097a04c707a5399df98a8678b1c685
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-10-19 10:57:55 +0530

    Time: 0 ms (100%), Space: 7.4 MB (87.54%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -1,33 +1,6 @@
 class Solution {
 public:
-
-    string funinv(string s)
-    {
-        string ans = "";
-        for(char x:s)
-        {
-            if(x=='1')
-            {
-                ans +="0";
-            }
-            else
-            {
-                ans+="1";
-            }
-        }
-    return ans;
-    }
-
-    char findKthBit(int n, int k) {
-        string prev = "0";
-        string curr;
-        for(int i =1;i<n;i++)
-        {
-            string inv = funinv(prev);
-            reverse(inv.begin(),inv.end());
-            curr = prev +"1"+ inv;
-            prev = curr;
-        }
-        return prev[k-1];
+    char findKthBit(int n, unsigned k) {
+        return k==1?'0':(k==bit_ceil(k)?'1':'0'+1-(findKthBit(n, bit_ceil(k)-k)-'0'));
     }
 };
