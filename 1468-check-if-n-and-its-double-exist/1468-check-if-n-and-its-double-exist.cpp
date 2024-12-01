8399147ed36c033bc483266467310486c1200b78
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-12-01 19:58:49 +0530

    Time: 2 ms (29.3%), Space: 13.4 MB (24.36%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        set<int>st(arr.begin(),arr.end());
-        for(int i =0 ; i<arr.size();i++)
-        {
-            if(st.count(2*arr[i]))
-            {
-                auto it = find(arr.begin(), arr.end(), 2*arr[i]);
-                if(distance(arr.begin(), it) == i ) continue;
-                return true;
+           for (int i = 0; i < arr.size(); i++) {
+            for (int j = 0; j < arr.size(); j++) {
+                if (i != j && arr[i] == 2 * arr[j]) {
+                    return true;
+                }
             }
-
         }
         return false;
     }
