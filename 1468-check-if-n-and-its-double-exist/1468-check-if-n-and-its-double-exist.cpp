a653be9d0c6a6cdc9a6e60be6280468f82272f46
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-12-01 20:04:15 +0530

    Time: 0 ms (100%), Space: 13.2 MB (41.61%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -1,13 +1,35 @@
 class Solution {
+private:
+    bool binarySearch(const vector<int>& arr, int target, int excludeIndex) {
+    int left = 0, right = arr.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target && mid != excludeIndex) {
+            return true;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}    
 public:
     bool checkIfExist(vector<int>& arr) {
-           for (int i = 0; i < arr.size(); i++) {
-            for (int j = 0; j < arr.size(); j++) {
-                if (i != j && arr[i] == 2 * arr[j]) {
-                    return true;
-                }
-            }
+      
+    sort(arr.begin(), arr.end());
+    
+    
+    for (int i = 0; i < arr.size(); ++i) {
+        
+        if (binarySearch(arr, 2 * arr[i], i)) {
+            return true;
         }
-        return false;
+        
+        if (arr[i] % 2 == 0 && binarySearch(arr, arr[i] / 2, i)) {
+            return true;
+        }
+    }
+    return false;
     }
 };
