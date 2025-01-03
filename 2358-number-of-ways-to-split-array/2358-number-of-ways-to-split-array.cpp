75c3bc88f226af8835ecf38af80e37edfa535494
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2025-01-03 21:59:35 +0530

    Time: 3 ms (58.82%), Space: 89.3 MB (79.17%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
-        vector<long long> insc(nums.size(), 0);
-        vector<long long> decs(nums.size(), 0);
-
-        
-        insc[0] = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
-            insc[i] = insc[i - 1] + nums[i];
+        int n = nums.size();
+        long long totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
         }
 
-        
-        decs[nums.size() - 1] = nums[nums.size() - 1];
-        for (int i = nums.size() - 2; i >= 0; i--) {
-            decs[i] = decs[i + 1] + nums[i];
-        }
-
-        
+        long long leftSum = 0;
         int res = 0;
-        for (int i = 0; i < nums.size() - 1; i++) {
-            if (insc[i] >= decs[i + 1]) {
-                res++;
+
+        for (int i = 0; i < n - 1; ++i) {
+            leftSum += nums[i];
+            long long rightSum = totalSum - leftSum;
+            if (leftSum >= rightSum) {
+                ++res;
             }
         }
 
