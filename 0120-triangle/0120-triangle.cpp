fac6de1699f7490776fcb440a1673848075c5c9c
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2025-05-02 00:18:03 +0530

    Time: 0 ms (100%), Space: 13.1 MB (62.16%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
+    int dfs(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& memo) {
+        if (i == triangle.size() - 1) return triangle[i][j]; 
+        
+        if (memo[i][j] != -1) return memo[i][j];  
+        
+        int down = dfs(i + 1, j, triangle, memo);
+        int downRight = dfs(i + 1, j + 1, triangle, memo);
+        
+        return memo[i][j] = triangle[i][j] + min(down, downRight);
+    }
+    
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<int>dp(triangle.back());
-
-
-        for(int row = n-2 ; row>=0;row--)
-        {
-            for(int i =0 ; i<=row;i++)
-            {
-                dp[i] = triangle[row][i]+min(dp[i],dp[i+1]);
-            }
-        }
-
-        return dp[0];
+        vector<vector<int>> memo(n, vector<int>(n, -1)); 
+        return dfs(0, 0, triangle, memo); 
     }
 };
