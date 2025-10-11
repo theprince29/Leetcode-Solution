906cfadac8ec1141216da10c7a455ba862f3b231
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2025-10-11 21:07:18 +0530

    Time: 135 ms (83.19%), Space: 128.7 MB (97.4%) - LeetHub

---
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maximumTotalDamage(vector<int>& power) {
+        long long dp[100001] = {}, max_dp = 0;
+        sort(begin(power), end(power));
+        for (int i = 0, j = 0; i < power.size(); ++i)
+            if (i == 0 || power[i] == power[i - 1])
+                dp[i + 1] = power[i] + dp[i];
+            else {
+                while (power[j] + 2 < power[i])
+                    max_dp = max(max_dp, dp[++j]);
+                dp[i + 1] = power[i] + max_dp;
+            }
+        return *max_element(begin(dp), end(dp));
+    }
+};
